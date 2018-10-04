b01aab17322ea43f46c94f0e36187577c00a05f8
Aurore <aurorem@bam.tech>
2018-10-04 22:23:31 +0200

    Fix symfony deployment

--- symfony/symfony-demo/webpack.config.js
+++ symfony/symfony-demo/webpack.config.js
@@ -1,17 +1,16 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    .setOutputPath('web/build/')
+    .setOutputPath('public/build/')
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .autoProvidejQuery()
     .autoProvideVariables({
-        "window.jQuery": "jquery",
         "window.Bloodhound": require.resolve('bloodhound-js'),
         "jQuery.tagsinput": "bootstrap-tagsinput"
     })
     .enableSassLoader()
-    .enableVersioning(false)
+    .enableVersioning()
     .createSharedEntry('js/common', ['jquery'])
     .addEntry('js/app', './assets/js/app.js')
     .addEntry('js/login', './assets/js/login.js')
